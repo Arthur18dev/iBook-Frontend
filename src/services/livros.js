dc5e1f5a76cdb59ff85bc58c68ceb6fd86ec011e
Luis Claudio Chaves <luis.claudio.meca@gmail.com>
2024-05-02 20:32:46 -0300

    Integração de livros com api

---
+++ src/services/livros.js
@@ -0,0 +1,20 @@
+import api from "./baseApi";
+
+
+
+const livrosServices = {
+    
+    getAllLivros: () => {
+        return api.get('livros')
+    },
+    getLivrosCategoria: (id) => {
+        return api.get('livrosPorCategoria', {
+            params: {
+                categoriaId: id
+            }
+        })
+    }
+}
+
+
+export default livrosServices
